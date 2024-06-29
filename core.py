c788aa388911a6be058cd405e1ddfd6afd4b64f0
Adam Dąbrowski <adam@codepy.pl>
2024-06-29 10:49:42 +0200

    dodano obsługę ścieżki bezwzględnej

--- core.py
+++ core.py
@@ -6,5 +6,16 @@ import io
 app = Flask(__name__)
 app.secret_key = 'Das5ahec#23a'
 
+BASE_PATH = '' # ZMIEŃ , podaj ścieżkę do folderu systemowego, który skrypt będzie przeglądać
+@app.route('/', defaults={'req_path': ''}, methods=['GET', 'POST'])
+@app.route('/<path:req_path>', methods=['GET', 'POST'])
+def index(req_path):
+    # definiujemy ścieżkę bezwzględną
+    absolute_path = os.path.join(BASE_PATH, req_path)
+
+    # Sprawdzamy, czy dana ścieżka istnieje
+    if not os.path.exists(absolute_path):
+        return abort(404)
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
